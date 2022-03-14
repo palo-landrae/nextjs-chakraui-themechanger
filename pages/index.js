d196965bcc56f0798a4326f8425596e306f7c078
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2022-03-14 11:38:19 +0100

    (working project) chakraui theme changer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Center, Text, HStack } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/button";
 
 const Home = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box>
-      <Text>Hello World!</Text>
-    </Box>
+    <Center>
+      <HStack>
+        <Text>Darkmode Button</Text>
+        <IconButton mt={4} aria-label="Toggle Mode" onClick={toggleColorMode}>
+          {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+        </IconButton>
+      </HStack>
+    </Center>
   );
 };
 
