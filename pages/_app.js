be5b57a84c66d24e6834e6e887b8c0b5ffef235b
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2022-03-14 14:07:50 +0100

    (feat) cookie, component-color-change

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "../theme";
+import { Chakra } from "../src/Chakra";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
+    <Chakra cookies={pageProps.cookies}>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </Chakra>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
-import { Center, Text, HStack } from "@chakra-ui/react";
-import { useColorMode } from "@chakra-ui/color-mode";
+import { Center, Text, HStack, VStack, Box } from "@chakra-ui/react";
+import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { IconButton } from "@chakra-ui/button";
 
 const Home = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue("red.500", "red.200");
+  const color = useColorModeValue("white", "gray.800");
+
   return (
-    <Center>
-      <HStack>
-        <Text>Darkmode Button</Text>
-        <IconButton mt={4} aria-label="Toggle Mode" onClick={toggleColorMode}>
-          {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-        </IconButton>
-      </HStack>
+    <Center h="full">
+      <VStack>
+        <HStack>
+          <Text>Darkmode Button</Text>
+          <IconButton mt={4} aria-label="Toggle Mode" onClick={toggleColorMode}>
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          </IconButton>
+        </HStack>
+        <Box w={72} h={72} mt={4} bg={bg} color={color} borderRadius={10}>
+          <Center h="full">This box will change color</Center>
+        </Box>
+      </VStack>
     </Center>
   );
 };
 
 export default Home;
+
+export { getServerSideProps } from "../src/Chakra";
